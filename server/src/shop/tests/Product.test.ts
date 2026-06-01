350f0ec1fda54e35288c01ada5e18a3eb5f96c9e
Antoliny0919 <antoliny0919@gmail.com>
2026-06-01 14:24:48 +0900

    feat: Product 모델 무결성 검증 추가

--- server/src/shop/tests/Product.test.ts
+++ server/src/shop/tests/Product.test.ts
@@ -15,4 +15,23 @@ describe("Product Tests", () => {
       }),
     );
   });
+
+  test.each(["", "           "])(
+    "상품명이 빈 값일때는 에러가 발생한다.",
+    (name) => {
+      expect(
+        () => new Product({ name: name, price: 10000, thumbnail: "some.jpg" }),
+      ).toThrow("상품명은 빈 값이어서는 안됩니다.");
+    },
+  );
+
+  test.each([0, -1, NaN, Infinity, -Infinity])(
+    "가격이 유효하지 않은 값일때는 에러가 발생한다.",
+    (price) => {
+      expect(
+        () =>
+          new Product({ name: "치킨", price: price, thumbnail: "chicken.png" }),
+      ).toThrow("가격은 0보다 큰 숫자여야 합니다.");
+    },
+  );
 });
