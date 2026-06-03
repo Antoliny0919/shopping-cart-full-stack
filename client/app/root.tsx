e44c1c66f0f9e5def7b0cb2f73b99834f2ec432e
Antoliny0919 <antoliny0919@gmail.com>
2026-06-03 16:06:26 +0900

    style: Global Style 추가

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -1,12 +1,21 @@
+import { Global, css } from "@emotion/react";
 import { Outlet, Scripts } from "react-router";
+
+const GlobalStyle = css`
+  body {
+    margin: 0;
+  }
+`;
 
 export default function App() {
   return (
     <html>
       <head>
         <link rel="icon" href="data:image/x-icon;base64,AA" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
+        <Global styles={GlobalStyle}></Global>
         <Outlet />
         <Scripts />
       </body>
