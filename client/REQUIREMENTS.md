9ea57c2dc3698f4e3c8b27ee2f5e7bce9cd46481
Antoliny0919 <antoliny0919@gmail.com>
2026-06-03 12:25:49 +0900

    build: storybook 의존성 추가

--- client/REQUIREMENTS.md
+++ client/REQUIREMENTS.md
@@ -24,7 +24,7 @@
   - [] 새로고침 시에도 선택된 상품은 유지되어야 한다. (useEffect of useSyncExternalStore, 부모/자식 컴포넌트 렌더링, 초기값 localStorage로부터)
 
 - [] 라이브러리
-  - [] Storybook
+  - [x] Storybook
   - [] MSW
   - [] RTL
   - [] Jest
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,46 @@
+/// <reference types="vitest/config" />
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+import { playwright } from "@vitest/browser-playwright";
+const dirname =
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [reactRouter()],
+  // https://v2.remix.run/docs/guides/vite#plugin-usage-with-other-vite-based-tools-eg-vitest-storybook
+  // Vitest나 Storybook처럼 Vite 설정 파일을 활용하는 다른 Vite 기반 도구들이 있지만,
+  // Remix Vite 플러그인은 이러한 도구들과 함께 사용하도록 설계되지 않았다.
+  plugins: [!process.env.STORYBOOK && reactRouter()].filter(Boolean),
+  test: {
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, ".storybook"),
+          }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: playwright({}),
+            instances: [
+              {
+                browser: "chromium",
+              },
+            ],
+          },
+        },
+      },
+    ],
+  },
 });
