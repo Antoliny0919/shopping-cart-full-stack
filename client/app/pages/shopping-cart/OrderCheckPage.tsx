62ba542c2dddd63f95f51e6cbdf6a0418549e769
Antoliny0919 <antoliny0919@gmail.com>
2026-06-03 11:56:16 +0900

    feat: 쇼핑카트, 주문확인 페이지 루트 컴포넌트 추가

---
+++ client/app/pages/shopping-cart/OrderCheckPage.tsx
@@ -0,0 +1,3 @@
+export default function OrderCheckPage() {
+  return <div>order check</div>
+}
--- client/app/routes.ts
+++ client/app/routes.ts
@@ -1,1 +1,8 @@
-export default [];
+import { type RouteConfig, route, prefix, index } from "@react-router/dev/routes";
+
+export default [
+  ...prefix("cart/", [
+    index("./pages/shopping-cart/ShoppingCartPage.tsx"),
+    route("check/", "./pages/shopping-cart/OrderCheckPage.tsx"),
+  ])
+] satisfies RouteConfig;
