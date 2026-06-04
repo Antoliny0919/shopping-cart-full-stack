de55ab27bf57287043e7d33e0253697b207543ea
Antoliny0919 <antoliny0919@gmail.com>
2026-06-05 01:00:56 +0900

    fix: 불필요한 console.log 제거

--- client/app/pages/shopping-cart/components/OrderCheckButton.tsx
+++ client/app/pages/shopping-cart/components/OrderCheckButton.tsx
@@ -3,7 +3,6 @@ import { Button } from "../../../commons/styles/Button";
 export default function OrderCheckButton(
   props: React.ButtonHTMLAttributes<HTMLButtonElement>,
 ) {
-  console.log(props);
   return (
     <Button type="button" {...props}>
       주문 확인
