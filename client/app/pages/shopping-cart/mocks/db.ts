bfaec7c50e04629defa2d5de0c9b1f43a9805f2f
Antoliny0919 <antoliny0919@gmail.com>
2026-06-04 09:10:14 +0900

    fix: 쇼핑카트 아이템을 가져올때 msw data 에서 가져오도록 수정

---
+++ client/app/pages/shopping-cart/mocks/db.ts
@@ -0,0 +1,58 @@
+import { factory, primaryKey } from "@mswjs/data";
+
+const db = factory({
+  cartItem: {
+    product_id: primaryKey(String),
+    quantity: Number,
+    product: {
+      name: String,
+      price: Number,
+      thumbnail: String,
+    },
+  },
+});
+
+const productItemData = [
+  {
+    product_id: "550e8400-e29b-41d4-a716-446655440000",
+    quantity: 2,
+    product: {
+      name: "치킨",
+      thumbnail: "/chicken.jpg",
+      price: 25000,
+    },
+  },
+  {
+    product_id: "f47ac10b-58cc-4372-a567-0e02b2c3d479",
+    quantity: 5,
+    product: {
+      name: "피자",
+      thumbnail: "/pizza.jpg",
+      price: 30000,
+    },
+  },
+  {
+    product_id: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+    quantity: 1,
+    product: {
+      name: "꿔바로우",
+      thumbnail: "/guobaorou.jpg",
+      price: 45000,
+    },
+  },
+  {
+    product_id: "1f0e3dad-99fb-40a2-9d57-8f3b7e6a2d1c",
+    quantity: 99,
+    product: {
+      name: "홈런볼",
+      thumbnail: "homerun-ball.jpg",
+      price: 1500,
+    },
+  },
+];
+
+for (const item of productItemData) {
+  db.cartItem.create(item);
+}
+
+export default db;
--- client/app/pages/shopping-cart/mocks/handlers.ts
+++ client/app/pages/shopping-cart/mocks/handlers.ts
@@ -1,45 +1,10 @@
 import { http, HttpResponse } from "msw";
 import { BASE_URL } from "../../../constants";
+import db from "./db";
 
 export const handlers = [
   http.get(`${BASE_URL}/api/cart/`, () => {
-    return HttpResponse.json([
-      {
-        product_id: "550e8400-e29b-41d4-a716-446655440000",
-        quantity: 2,
-        product: {
-          name: "치킨",
-          thumbnail: "/chicken.jpg",
-          price: 25000,
-        },
-      },
-      {
-        product_id: "f47ac10b-58cc-4372-a567-0e02b2c3d479",
-        quantity: 5,
-        product: {
-          name: "피자",
-          thumbnail: "/pizza.jpg",
-          price: 30000,
-        },
-      },
-      {
-        product_id: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-        quantity: 1,
-        product: {
-          name: "꿔바로우",
-          thumbnail: "/guobaorou.jpg",
-          price: 45000,
-        },
-      },
-      {
-        product_id: "1f0e3dad-99fb-40a2-9d57-8f3b7e6a2d1c",
-        quantity: 99,
-        product: {
-          name: "홈런볼",
-          thumbnail: "homerun-ball.jpg",
-          price: 1500,
-        },
-      },
-    ]);
+    const data = db.cartItem.getAll();
+    return HttpResponse.json(data);
   }),
 ];
