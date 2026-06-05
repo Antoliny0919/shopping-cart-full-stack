c367897c58733b7d3e2441b26f157273e4e68417
Antoliny0919 <antoliny0919@gmail.com>
2026-06-05 19:29:15 +0900

    refactor 불필요한 OrderSubmitButton 컴포넌트 제거

--- client/app/pages/shopping-cart/components/OrderSubmitButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "../../../commons/styles/Button";
-
-export default function OrderSubmitButton() {
-  return (
-    <Button type="button" disabled>
-      결제 하기
-    </Button>
-  );
-}
