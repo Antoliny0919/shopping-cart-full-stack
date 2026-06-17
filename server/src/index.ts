f30dd4be69b375081030cbe7ec3d5f9c1943abd5
Antoliny0919 <antoliny0919@gmail.com>
2026-06-17 16:46:18 +0900

    feat: 시드 쿠폰 데이터 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,12 @@ import {
   InMemoryTempOrderRepository,
 } from "./shop/repositories/InMemoryRepositories.js";
 import Product from "./shop/models/Product.js";
+import {
+  AmountDiscountCoupon,
+  BonusCoupon,
+  FreeShippingCoupon,
+  RateDiscountCoupon,
+} from "./shop/models/Coupon.js";
 
 const PORT = process.env.PORT ?? 3000;
 
@@ -17,20 +23,33 @@ const productRepository = new InMemoryProductRepository();
 const cartRepository = new InMemoryCartRepository();
 const tempOrderRepository = new InMemoryTempOrderRepository();
 
-const seedData = [
-  { name: "치킨", thumbnail: "/chicken.png", price: 25000, quantity: 2 },
-  { name: "피자", thumbnail: "/pizza.jpg", price: 30000, quantity: 5 },
-  { name: "꿔바로우", thumbnail: "/guobaorou.jpg", price: 45000, quantity: 1 },
-  {
-    name: "홈런볼",
-    thumbnail: "/home-run-ball.jpg",
-    price: 1500,
-    quantity: 99,
-  },
-];
+const seedData = {
+  products: [
+    { name: "치킨", thumbnail: "/chicken.png", price: 25000, quantity: 2 },
+    { name: "피자", thumbnail: "/pizza.jpg", price: 30000, quantity: 5 },
+    {
+      name: "꿔바로우",
+      thumbnail: "/guobaorou.jpg",
+      price: 45000,
+      quantity: 1,
+    },
+    {
+      name: "홈런볼",
+      thumbnail: "/home-run-ball.jpg",
+      price: 1500,
+      quantity: 99,
+    },
+  ],
+  coupons: [
+    new AmountDiscountCoupon({ policies: [], discountPrice: 5000 }),
+    // new BonusCoupon(),
+    // new FreeShippingCoupon(),
+    // new RateDiscountCoupon(),
+  ],
+};
 
 const cart = cartRepository.get();
-for (const { quantity, ...productData } of seedData) {
+for (const { quantity, ...productData } of seedData["products"]) {
   const product = new Product(productData);
   productRepository.save(product.getId(), product);
   cart.updateItemByProductId(product.getId(), quantity);
