f62a6e4190efd576199569bc8ae61ee52def9236
Antoliny0919 <antoliny0919@gmail.com>
2026-06-20 16:05:11 +0900

    test: 존재하지 않는 임시 주문서를 가져오려는 요청에 404 에러 반환 테스트 케이스 추가

--- server/src/shop/tests/Api.test.ts
+++ server/src/shop/tests/Api.test.ts
@@ -479,6 +479,15 @@ describe("임시 주문서 API 테스트", () => {
     });
   });
 
+  test("존재하지 않은 임시주문서를 가져오면 404 에러가 발생한다.", async () => {
+    const res = await request(app).get(`/api/orders/unknown/`);
+    expect(res.status).toBe(404);
+    expect(res.body).toEqual({
+      code: "RESOURCE_NOT_FOUND",
+      message: "요청한 리소스를 찾을 수 없습니다.",
+    });
+  });
+
   test("특정 임시 주문서를 수정한다.", async () => {
     const id = tempOrder.getId();
     const res = await request(app)
