a0c7f6f738f40f18fa7c116addc045f5a9cc95af
Antoliny0919 <antoliny0919@gmail.com>
2026-06-20 22:50:58 +0900

    feat: 주문확인 페이지 주문한 상품의 정보를 렌더링하는 컴포넌트 추가

---
+++ client/app/pages/order-check/components/Section.tsx
@@ -0,0 +1,74 @@
+import { useLocation } from "react-router";
+import styled from "@emotion/styled";
+import { Button } from "../../../commons/styles/Button";
+import OrderItemList from "./OrderItemList";
+
+export default function Section() {
+  const { totalItems, totalQuantity, totalPrice } = useLocation().state;
+  const items = [
+    {
+      product_id: "123",
+      quantity: 2,
+      product: {
+        name: "상품이름A",
+        price: 35000,
+        thumbnail: "/chicken.png",
+      },
+    },
+    {
+      product_id: "456",
+      quantity: 5,
+      product: {
+        name: "상품이름B",
+        price: 50000,
+        thumbnail: "/pizza.jpg",
+      },
+    },
+  ];
+  return (
+    <SectionLayout>
+      <Title>주문 확인</Title>
+      <SubText>
+        총 {totalItems}종류의 상품 {totalQuantity}개를 주문합니다.
+      </SubText>
+      <SubText>최종 결제 금액을 확인해 주세요.</SubText>
+      <OrderItemList items={items}></OrderItemList>
+      <Button type="button" disabled={true}>
+        결제하기
+      </Button>
+    </SectionLayout>
+  );
+}
+
+const SectionLayout = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  padding: 1.5rem;
+  margin-bottom: 4rem;
+  overflow: scroll;
+`;
+
+const Title = styled.h2`
+  font-weight: 700;
+  font-size: 24px;
+`;
+
+const SubText = styled.p`
+  margin: 0;
+  font-weight: 500;
+  font-size: 12px;
+  margin: 2px 0;
+`;
+
+// const TotalPriceLabel = styled.p`
+//   margin: 1.5rem 0;
+//   font-weight: 700;
+//   font-size: 16px;
+// `;
+
+// const TotalPrice = styled.p`
+//   margin: 0;
+//   font-weight: 700;
+//   font-size: 24px;
+// `;
--- client/app/pages/shopping-cart/OrderCheckPage.tsx
+++ client/app/pages/shopping-cart/OrderCheckPage.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 import { Link } from "react-router";
 import GoBack from "../../commons/images/go-back.svg?react";
 import Navigation from "../../commons/components/Navigation";
-import Section from "../order-check/Section";
+import Section from "../order-check/components/Section";
 
 export default function OrderCheckPage() {
   return (
