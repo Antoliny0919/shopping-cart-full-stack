9b89529f097f9052ad7c82ec44be49d4b5a7c22b
Antoliny0919 <antoliny0919@gmail.com>
2026-06-22 04:16:29 +0900

    fix: 잘못된 패키지 사용 수정

--- client/app/commons/components/Spinner.tsx
+++ client/app/commons/components/Spinner.tsx
@@ -1,4 +1,5 @@
-import styled, { keyframes } from "@emotion/styled";
+import styled from "@emotion/styled";
+import { keyframes } from "@emotion/react";
 
 export default function Spinner() {
   return (
